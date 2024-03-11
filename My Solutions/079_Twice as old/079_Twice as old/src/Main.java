5d8f2c0aefbe771563166daa6a35efee47560c78
iambeno1 <benonygabriel40@gmail.com>
2024-03-11 21:45:15 +0700

    day 79/366

---
+++ My Solutions/079_Twice as old/079_Twice as old/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int yearsAgo;
+        try {
+            yearsAgo = TwiceAsOld.TwiceAsOld(30, 5);
+            System.out.println("Years ago: " + yearsAgo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

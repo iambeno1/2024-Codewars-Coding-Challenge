73dba74ae30dafaf02bf50c8c2b7f54e42605b60
iambeno1 <benonygabriel40@gmail.com>
2024-03-29 18:31:11 +0700

    day 90/366

---
+++ My Solutions/090_Array plus array.py
@@ -0,0 +1,24 @@
+"""
+Codewars Coding Challenge 
+
+Day 90/366
+
+Level 8kyu Python
+
+Array plus array
+
+I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.
+
+def array_plus_array(arr1,arr2):
+    pass
+
+
+https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/train/python
+
+"""
+
+# My Solutions
+def array_plus_array(arr1,arr2):
+    return sum(arr1 + arr2)
